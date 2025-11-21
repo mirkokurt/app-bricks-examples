84ad13fe6601bdc657bbd4727fb0a4173451fbd7
Stefano Torneo <s.torneo@arduino.cc>
2025-11-21 18:48:59 +0100

    fix copyright note

--- examples/color-your-leds/assets/app.js
+++ examples/color-your-leds/assets/app.js
@@ -1,8 +1,8 @@
-// SPDX-FileCopyrightText: Copyright (C) 2025 ARDUINO SA <http://www.arduino.cc>
+// SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
 //
 // SPDX-License-Identifier: MPL-2.0
 
-// SPDX-FileCopyrightText: Copyright (C) 2025 ARDUINO SA <http://www.arduino.cc>
+// SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
 //
 // SPDX-License-Identifier: MPL-2.0
 
